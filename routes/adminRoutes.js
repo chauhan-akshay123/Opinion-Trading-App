7653df4e9154d5c82664a3582f098ee98e7e2818
Akshay Chauhan <chauhanakshay395@gmail.com>
2025-03-10 23:24:06 +0530

    Added adminRoutes

---
+++ routes/adminRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { authMiddleware } = require("../middleware/authMiddleware");
+const { adminMiddleware } = require("../middleware/roleMiddleware");
+
+const router = express.Router();
+
+router.get("/dashboard", authMiddleware, adminMiddleware, (req, res) => {
+  res.json({ message: "Welcome to Admin Dashboard" });
+});
+
+modul.exports = router;
